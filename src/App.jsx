a1885ee5ce7820f27f73d183eb6dba7872a6f506
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-12 23:29:22 +0545

    Top section added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TopSection from "./components/TopSection";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main className="min-h-screen p-10">
+        <TopSection />
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TopSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const TopSection = () => {
+  return (
+    <section>
+      <p className="text-xl">Explore our classes and master trending skills!</p>
+      <p className="mt-4 font-bold text-3xl ">
+        Dive Into <span className="text-[#1da077]">What’s Hot Right Now!</span>{" "}
+      </p>
+    </section>
+  );
+};
+
+export default TopSection;
